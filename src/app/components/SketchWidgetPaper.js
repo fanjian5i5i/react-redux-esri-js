e60dbb31dfb41e89857f7489be3651383f12d50f
fjgispc <jian.fan@boston.gov>
2019-07-11 11:10:15 -0700

    age chart working

--- src/app/components/SketchWidgetPaper.js
+++ src/app/components/SketchWidgetPaper.js
@@ -8,6 +8,8 @@ import ButtonGroup from '@material-ui/core/ButtonGroup';
 import { connect } from 'react-redux';
 import { updateView,updateMap,updateSelected } from '../redux/actions';
 
+import {HorizontalBar} from 'react-chartjs-2';
+
 import Icon from '@material-ui/core/Icon';
 import IconButton from '@material-ui/core/IconButton';
 
@@ -27,6 +29,7 @@ const useStyles = makeStyles(theme => ({
 function SketchWidgetPaper(props) {
   const classes = useStyles();
   const [selected, setSelected] = useState("");
+  const [ageData, setAgeData] = useState("");
 
 
 
@@ -55,9 +58,28 @@ function SketchWidgetPaper(props) {
   //   return { data: censusGeoJSON};
   // };
 
+  let processAgeData = (data) =>{
+    console.log(data)
+    let result = [];
+    // data.forEach(variable=>{
+    //   result.push(variable);
+    // });
+    for (var key in data) {
+        if (data.hasOwnProperty(key)) {
+            if(key!='county'&&key!='state'&&key!='tract'){
+              result.push(data[key])
+            }
+        }
+    }
+    console.log(result)
+    return result
+  }
+
 
   let center = { lat: 42.3601, lng: -71.0589 };
-  let values = ["B00001_001E", "B01001_001E", "B08303_001E"];
+  let values = ["B01001_003E", "B01001_004E", "B01001_005E","B01001_006E", "B01001_007E", "B01001_008E","B01001_009E", "B01001_010E",
+"B01001_011E", "B01001_012E", "B01001_013E","B01001_014E", "B01001_015E", "B01001_016E","B01001_017E", "B01001_018E",
+"B01001_019E", "B01001_020E", "B01001_021E","B01001_022E", "B01001_023E", "B01001_024E","B01001_025E"];
   let Args = {
       "vintage": 2016,
       "geoHierarchy": {
@@ -66,7 +88,7 @@ function SketchWidgetPaper(props) {
       },
       "sourcePath": ["acs", "acs5"],
       "values": values,
-      "geoResolution": "500k",
+      // "geoResolution": "500k",
     };
   let handleClear =()=> {
       props.mapState.view.graphics.removeAll();
@@ -78,8 +100,26 @@ function SketchWidgetPaper(props) {
       //   console.log(res)
       // });
       census(Args,
-        (err, res) => console.log(res)
-      )
+        (err, res) => {
+          console.log(res);
+
+          const data = {
+            labels: ['0-5', '5-10', '10-15', '15-20', '20-25', '25-30', '35-40','45-50','50-55','55-60','60-65','65-70','70-75','75-80','80-85','>85'],
+            datasets: [
+              {
+                label: 'Male',
+                backgroundColor: 'rgba(255,99,132,0.2)',
+                borderColor: 'rgba(255,99,132,1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                hoverBorderColor: 'rgba(255,99,132,1)',
+                data: processAgeData(res[0])
+              }
+            ]
+          };
+
+          setAgeData(data)
+        })
     }
 
 
@@ -106,6 +146,19 @@ function SketchWidgetPaper(props) {
           </Grid>
         </Grid>
       </Grid>
+
+
+      <div>
+        <h2>Age/Sex Distribution</h2>
+        <HorizontalBar
+          data={ageData != ""?ageData:[]}
+          width={100}
+          height={150}
+          options={{
+            maintainAspectRatio: true
+          }}
+        />
+      </div>
       </Paper>
     </div>
   );
