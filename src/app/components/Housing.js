451c4a2c414808fecd5d92cdae4efdeba3c63ae4
FJ <fanjian5i5i@gmail.com>
2020-03-20 17:54:16 -0400

    CEN-13, CEN-12| pie chart and table created need ui updates

--- src/app/components/Housing.js
+++ src/app/components/Housing.js
@@ -1,65 +1,96 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
 import ListItemText from '@material-ui/core/ListItemText';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import Collapse from '@material-ui/core/Collapse';
 
-import NativityChart from './NativityChart';
-import AgeChart from './AgeChart'
-
+import HousingChart from './HousingChart';
+const census = require("citysdk");
 import { connect } from 'react-redux';
-const useStyles = makeStyles(theme => ({
-  title:{
-    color:"#003c50"
-  },
-}));
 
-function Housing(props) {
-  const classes = useStyles();
-  const [nativityOpen, setNativityOpen] = React.useState(false);
-  const [value, setValue] =React.useState(1);
-  const [ageOpen, setAgeOpen] = React.useState(true);
-  const handleClickCode = (code) => () => {
-    let that = this;
-    switch (code) {
-      case "nativity":
-        setNativityOpen(!nativityOpen);
-        break;
-      case "age":
-        setAgeOpen(!ageOpen);
-        break;
-      default:
-
+  
+class Housing extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      loading:true,
+      data:null,
     }
+  }
 
-  };
+  componentDidMount(){
+    if(this.props.mapState.selected){
+      this._loadAsyncData("group(B25003)")
+    }
+  }
+  componentDidUpdate(prevProps, prevState){
+    // console.log(prevProps.mapState.selected)
+    // console.log(this.props.mapState.selected)
+    if(prevProps.mapState.selected !== this.props.mapState.selected){
+      this._loadAsyncData("group(B25003)")
+    }
+  }
+  processIncomeData (data){
+    let temp1 = parseInt(data["B25003_002E"]);
+    let temp2 = parseInt(data["B25003_003E"]);
+    // let temp3 = parseInt(data["B19001_006E"]) +parseInt(data["B19001_007E"]);;
 
-  React.useEffect(()=>{
-    setValue(2);
-    fetch("http://dummy.restapiexample.com/api/v1/employees").then((result)=>{
-      console.log(result)
-    })
+    return [{name:"Owner Occupied",y:temp1},{name:"Renter Occupied",y:temp2}];
+  } 
+  _loadAsyncData(values){
+    let that = this;
+    let center = { lat: 42.3601, lng: -71.0589 };
+    let Args = this.props.mapState.layer !== "city" ? {
+          "vintage": 2017,
+          "geoHierarchy": {
+            "county": center,
+            "tract": this.props.mapState.selected
+          },
+          "sourcePath": ["acs", "acs5"],
+          "values": [values],
+          // "geoResolution": "500k",
+        }:{
+          "vintage": 2017,
+          "geoHierarchy": {
+            "county": center,
+            "county subdivision":"07000"
+          },
+          "sourcePath": ["acs", "acs5"],
+          "values": [values],
+        }
+        census(Args,
+          (err, res) => {
+            console.log(res);
+            let keys = Object.keys(res[0]);
+            // let keyLength = keys.length - 3;
+            let tempObj = {}
+            keys.forEach(key =>{
 
-  },[])
+              let temp = 0;
+              for(var i = 0; i< res.length ; i++ ){
+
+                temp += parseInt(res[i][key]);
+              }
+              tempObj[key]  = temp;
+
+            });
+              that.setState({data:that.processIncomeData(tempObj)})
+
+
+
+          });
+
+  }
+  
+  render(){
   return (
     <div>
-      <ListItem button onClick={handleClickCode("nativity")} className={classes.title}>
-          <ListItemText inset primary="Housing" className={classes.title}/>
-            {nativityOpen ? <ExpandLess /> : <ExpandMore />}
+      <ListItem >
+          <ListItemText inset primary="Housing Tenure"/>
+          <HousingChart data={this.state.data}/>
           </ListItem>
-          <Divider />
-          <Collapse in={nativityOpen} timeout="auto">
 
-          {value}
-
-
-          </Collapse>
-          <Divider/>
     </div>
-  );
+  )}
 }
 
 const mapStateToProps = state => ({
---
+++ src/app/components/HousingChart.js
@@ -0,0 +1,97 @@
+import React, { useEffect , useState} from "react";
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+let HousingChart = (props) =>{
+  const [data,setData] = useState([]);
+  useEffect(()=>{
+    console.log(props.data);
+    setData(props.data)
+  },[props.data])
+  const options = {
+    title: {
+      text: 'Housing Tenure'
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+  },
+  accessibility: {
+    point: {
+        valueSuffix: '%'
+    }
+    },
+    plotOptions: {
+      pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+              enabled: true,
+              format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+          }
+      }
+    },
+    chart: {
+      type: 'pie'
+    },
+    series: [{
+      name:"Household Income",
+      colorByPoint: true,
+      data: props.data
+    }],
+    // dataLabels: {
+    //   enabled: true,
+    //   rotation: -90,
+    //   color: '#FFFFFF',
+    //   align: 'right',
+    //   format: '{point.y:.1f}', // one decimal
+    //   y: 10, // 10 pixels down from the top
+    //   style: {
+    //       fontSize: '13px',
+    //       fontFamily: 'Verdana, sans-serif'
+    //   }
+    // }
+  }
+  return (
+    <div>
+      
+      <Paper style={{paddingLeft:80,paddingRight:80}}>
+        <Table size="small">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="right">
+                      Housing Tenure
+                    </TableCell>
+                    <TableCell>
+                      Counts
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {
+                    props.data?
+                    props.data.map(record=>(
+                      <TableRow key={record.name}>
+                            <TableCell align="right">{record.name}</TableCell>
+                            <TableCell>{record.y}</TableCell>
+                          </TableRow>
+                    ))
+                    :""
+                  }
+                </TableBody>
+        </Table>
+      </Paper>
+    <HighchartsReact
+      highcharts={Highcharts}
+      options={options}
+    />
+    </div>
+  )
+}
+
+
+export default HousingChart
