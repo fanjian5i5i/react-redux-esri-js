a4db5c3c805506f1cea629f9add4b28d608684f5
jian fan <fanjian5i5i@gmail.com>
2020-03-27 15:58:58 -0400

    CEN-10| Poverty Chart and Table

--- src/app/components/Income.js
+++ src/app/components/Income.js
@@ -5,7 +5,9 @@ import Divider from '@material-ui/core/Divider';
 import ListItemText from '@material-ui/core/ListItemText';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
-import Collapse from '@material-ui/core/Collapse';import IncomeChart from './IncomeChart';
+import Collapse from '@material-ui/core/Collapse';
+import IncomeChart from './IncomeChart';
+import PovertyChart from './PovertyChart';
 const census = require("citysdk");
 import { connect } from 'react-redux';
 // const useStyles = makeStyles(theme => ({
@@ -25,14 +27,13 @@ class Income extends React.Component {
     this.state = {
       loading:true,
       incomeData:null,
+      povertyDat:null,
       selected:props.mapState.selected,
       houseOpen:true,
       povertyOpen:false
     }
   }
-  processCensusData(selected){
-    console.log(selected)
-  }
+
   static getDerivedStateFromProps(props,state){
     console.log(props.mapState);
     // console.log(this.props.mapState)
@@ -50,7 +51,6 @@ class Income extends React.Component {
   }
   processIncomeData (data){
 
-    let result = []
     let les14999 = parseInt(data["B19001_002E"]) +parseInt(data["B19001_003E"]);
     let les24999 = parseInt(data["B19001_004E"]) +parseInt(data["B19001_005E"]);
     let les34999 = parseInt(data["B19001_006E"]) +parseInt(data["B19001_007E"]);;
@@ -63,6 +63,50 @@ class Income extends React.Component {
     console.log(other)
     return [["0-14,999",les14999],["15,000-24,999",les24999],["25,000-34,999",les34999],["35,000-49,999",les49999],["50,000-74,999",les74999],["75,000-99,999",les99999],["99,999-149,999",les149999],[">150,000",other]];
   }
+  processPovertyData(data){
+    //Under 18 years
+    //((B17001_004E + B17001_018E) + (B17001_005E + B17001_019E) + (B17001_006E + B17001_020E) + (B17001_007E + B17001_021E) + (B17001_008E + B17001_022E) + (B17001_009E + B17001_023E)) / 
+    //((B17001_004E + B17001_018E + B17001_033E + B17001_047E) + (B17001_005E + B17001_019E + B17001_034E + B17001_048E) + (B17001_006E + B17001_020E + B17001_035E + B17001_049E) + 
+    //(B17001_007E + B17001_021E + B17001_036E + B17001_050E) + (B17001_008E + B17001_022E + B17001_037E + B17001_051E) + (B17001_009E + B17001_023E + B17001_038E + B17001_052E))
+    let temp1 = (parseInt(data["B17001_004E"]) +parseInt(data["B17001_018E"])+
+    parseInt(data["B17001_005E"]) +parseInt(data["B17001_019E"])+
+    parseInt(data["B17001_006E"]) +parseInt(data["B17001_020E"])+
+    parseInt(data["B17001_007E"]) +parseInt(data["B17001_021E"])+
+    parseInt(data["B17001_008E"]) +parseInt(data["B17001_022E"])+
+    parseInt(data["B17001_009E"]) +parseInt(data["B17001_023E"]))/
+    (parseInt(data["B17001_004E"]) +parseInt(data["B17001_018E"])+parseInt(data["B17001_033E"]) +parseInt(data["B17001_047E"])+
+    parseInt(data["B17001_005E"]) +parseInt(data["B17001_019E"])+parseInt(data["B17001_034E"]) +parseInt(data["B17001_048E"])+
+    parseInt(data["B17001_006E"]) +parseInt(data["B17001_020E"])+parseInt(data["B17001_035E"]) +parseInt(data["B17001_049E"])+
+    parseInt(data["B17001_007E"]) +parseInt(data["B17001_021E"])+parseInt(data["B17001_036E"]) +parseInt(data["B17001_050E"])+
+    parseInt(data["B17001_008E"]) +parseInt(data["B17001_022E"])+parseInt(data["B17001_037E"]) +parseInt(data["B17001_051E"])+
+    parseInt(data["B17001_009E"]) +parseInt(data["B17001_023E"])+parseInt(data["B17001_038E"]) +parseInt(data["B17001_052E"]));
+
+    //18 to 64 years
+    //((B17001_010E + B17001_024E) + (B17001_011E + B17001_025E) + (B17001_012E + B17001_026E) + (B17001_013E + B17001_027E) + (B17001_014E + B17001_028E)) / 
+    //((B17001_010E + B17001_024E + B17001_039E + B17001_053E) + (B17001_011E + B17001_025E + B17001_040E + B17001_054E) + (B17001_012E + B17001_026E + B17001_041E + B17001_055E) + 
+    //(B17001_013E + B17001_027E + B17001_042E + B17001_056E) + (B17001_014E + B17001_028E + B17001_043E + B17001_057E))
+    let temp2 = (parseInt(data["B17001_010E"]) +parseInt(data["B17001_024E"])+
+    parseInt(data["B17001_011E"]) +parseInt(data["B17001_025E"])+
+    parseInt(data["B17001_012E"]) +parseInt(data["B17001_026E"])+
+    parseInt(data["B17001_013E"]) +parseInt(data["B17001_027E"])+
+    parseInt(data["B17001_014E"]) +parseInt(data["B17001_028E"]))/
+    (parseInt(data["B17001_010E"]) +parseInt(data["B17001_024E"])+parseInt(data["B17001_039E"]) +parseInt(data["B17001_053E"])+
+    parseInt(data["B17001_011E"]) +parseInt(data["B17001_025E"])+parseInt(data["B17001_040E"]) +parseInt(data["B17001_054E"])+
+    parseInt(data["B17001_012E"]) +parseInt(data["B17001_026E"])+parseInt(data["B17001_041E"]) +parseInt(data["B17001_055E"])+
+    parseInt(data["B17001_013E"]) +parseInt(data["B17001_027E"])+parseInt(data["B17001_042E"]) +parseInt(data["B17001_056E"])+
+    parseInt(data["B17001_014E"]) +parseInt(data["B17001_028E"])+parseInt(data["B17001_043E"]) +parseInt(data["B17001_057E"]));
+    //65 years and over
+    //((B17001_015E + B17001_029E) + (B17001_016E + B17001_030E)) / 
+    //((B17001_015E + B17001_029E + B17001_044E + B17001_058E) + (B17001_016E + B17001_030E + B17001_045E + B17001_059E))
+    let temp3 = (parseInt(data["B17001_015E"]) +parseInt(data["B17001_029E"])+
+    parseInt(data["B17001_016E"]) +parseInt(data["B17001_030E"]))/
+    (parseInt(data["B17001_015E"]) +parseInt(data["B17001_029E"])+parseInt(data["B17001_044E"]) +parseInt(data["B17001_058E"])+
+    parseInt(data["B17001_016E"]) +parseInt(data["B17001_030E"])+parseInt(data["B17001_045E"]) +parseInt(data["B17001_059E"]));
+
+
+    return [["Under 18 years",parseFloat((temp1*100).toString().substring(0,5))],["18 to 64 years",parseFloat((temp2*100).toString().substring(0,5))],["65 years and over",parseFloat((temp3*100).toString().substring(0,5))]];
+  
+  }
   shouldComponentUpdate(nextProps, nextState){
     // console.log(this.state);
     // console.log(nextState);
@@ -70,18 +114,21 @@ class Income extends React.Component {
   }
   componentDidMount(){
     if(this.props.mapState.selected){
-      this._loadAsyncData("group(B19001)")
+      this._loadAsyncData("group(B19001)","income")
+      this._loadAsyncData("group(B17001)","poverty")
     }
   }
   componentDidUpdate(prevProps, prevState){
     // console.log(prevProps.mapState.selected)
     // console.log(this.props.mapState.selected)
     if(prevProps.mapState.selected !== this.props.mapState.selected){
-      this._loadAsyncData("group(B19001)")
+      this._loadAsyncData("group(B19001)","income")
+      this._loadAsyncData("group(B17001)","poverty")
+      
     }
   }
 
-  _loadAsyncData(values){
+  _loadAsyncData(values,category){
     let that = this;
     let center = { lat: 42.3601, lng: -71.0589 };
     let Args = this.props.mapState.layer !== "city" ? {
@@ -118,7 +165,18 @@ class Income extends React.Component {
               tempObj[key]  = temp;
 
             });
-              that.setState({incomeData:that.processIncomeData(tempObj)})
+
+              switch(category){
+                case 'income':
+                  that.setState({incomeData:that.processIncomeData(tempObj)})
+                  break;
+                case 'poverty':
+                  that.setState({povertyData:that.processPovertyData(tempObj)})
+                  break;
+                // default:
+                  
+
+              }
 
 
 
@@ -165,7 +223,7 @@ class Income extends React.Component {
           <Divider />
           <Collapse in={this.state.povertyOpen} timeout="auto">
 
-
+          <PovertyChart data={this.state.povertyData}/>                                 
 
           </Collapse>
     </div>
--- src/app/components/PovertyChart.js
+++ src/app/components/PovertyChart.js
@@ -1,48 +1,100 @@
-import React from 'react';
+import React, { useEffect , useState} from "react";
 import Highcharts from 'highcharts'
-import HighchartsReact from 'highcharts-react-official'
-
-const PovertyChart = (props) =>{
-    // let data = props.data;
-    const options = {
-        title: {
-          text: ''
-        },
-        xAxis: {
-          type: 'category',
-          labels: {
-              rotation: -45,
-              style: {
-                  fontSize: '13px',
-                  fontFamily: 'Verdana, sans-serif'
-              }
-          }
+import HighchartsReact from 'highcharts-react-official';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+let PovertyChart = (props) =>{
+  const [data,setData] = useState([]);
+  useEffect(()=>{
+    console.log(props.data);
+    setData(props.data)
+  },[props.data])
+  const options = {
+    title: {
+      text: 'Poverty Rate'
+    },
+      xAxis: {
+        type: 'category',
+        labels: {
+            rotation: -45,
+            style: {
+                fontSize: '13px',
+                fontFamily: 'Verdana, sans-serif'
+            }
+        }
+    },
+      chart: {
+        type: 'column'
       },
-        chart: {
-          type: 'column'
-        },
-        series: [{
-          name:"Poverty",
-          data: props.data
-        }],
-        dataLabels: {
-          enabled: true,
-          rotation: -90,
-          color: '#FFFFFF',
-          align: 'right',
-          format: '{point.y:.1f}', // one decimal
-          y: 10, // 10 pixels down from the top
-          style: {
-              fontSize: '13px',
-              fontFamily: 'Verdana, sans-serif'
-          }
+      series: [{
+        name:"Poverty rate",
+        data: data?data:[]
+      }],
+      dataLabels: {
+        enabled: true,
+        rotation: -90,
+        color: '#FFFFFF',
+        align: 'right',
+        format: '{point.y:.1f}', // one decimal
+        y: 10, // 10 pixels down from the top
+        style: {
+            fontSize: '13px',
+            fontFamily: 'Verdana, sans-serif'
         }
       }
-    return (
-<HighchartsReact
-            highcharts={Highcharts}
-            options={options}
-          />
-    )
+    // dataLabels: {
+    //   enabled: true,
+    //   rotation: -90,
+    //   color: '#FFFFFF',
+    //   align: 'right',
+    //   format: '{point.y:.1f}', // one decimal
+    //   y: 10, // 10 pixels down from the top
+    //   style: {
+    //       fontSize: '13px',
+    //       fontFamily: 'Verdana, sans-serif'
+    //   }
+    // }
+  }
+  return (
+    <div>
+      
+      <Paper style={{paddingLeft:80,paddingRight:80}}>
+        <Table size="small">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="right">
+                      Poverty Rate
+                    </TableCell>
+                    <TableCell>
+                      Percentage(%)
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {
+                    props.data?
+                    props.data.map(record=>(
+                      <TableRow key={record[0]}>
+                            <TableCell align="right">{record[0]}</TableCell>
+                            <TableCell>{record[1]}</TableCell>
+                          </TableRow>
+                    ))
+                    :""
+                  }
+                </TableBody>
+        </Table>
+      </Paper>
+    <HighchartsReact
+      highcharts={Highcharts}
+      options={options}
+    />
+    </div>
+  )
 }
-export default PovertyChart;
+
+
+export default PovertyChart
